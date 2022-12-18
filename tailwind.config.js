cd1dd84e108f2732137048570d1bafd9aa88a1a2
alireza khanamani <khanamani1998@gmail.com>
2022-12-18 13:27:24 +0330

    define key frames

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,9 +28,41 @@ module.exports = {
           "0%, 100%": { transform: "translateX(2em)" },
           "50%": { transform: "translateX(4em)" },
         },
+        leftToRight: {
+          "0%": { transform: "translate(0)" },
+          "25%": { transform: "translateY(100%)" },
+          "50%": {
+            transform: "translateY(100%) translateX(100%)",
+            background: "#00F",
+          },
+          "75%": { transform: "translateX(100%)" },
+          "100%": { transform: "translate(0)" },
+        },
+        sidebarOpen: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        sidebarClose: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+        opacity: {
+          "0%": { opacity: 0, visibility: "hidden" },
+          "50%": { opacity: 0, visibility: "hidden" },
+          "100%": { opacity: 1, visibility: "visible" },
+        },
+        opacityR: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0, visibility: "hidden" },
+        },
       },
       animation: {
         wiggle: "wiggle 1s ease-in-out infinite",
+        leftToRight: "leftToRight 3s ease-in-out forwards infinite",
+        sidebarOpen: "sidebarOpen .5s ease-in-out forwards",
+        sidebarClose: "sidebarClose .5s ease-in-out forwards",
+        opacity: "opacity 1s ease-in-out forwards",
+        opacityR: "opacityR 1s ease-in-out forwards",
       },
     },
   },
