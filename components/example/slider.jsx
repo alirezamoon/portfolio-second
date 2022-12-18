caffbf98592e9ea420a2801453741c62269ff173
alireza khanamani <khanamani1998@gmail.com>
2022-12-18 16:29:26 +0330

    slider type 2

--- components/example/slider.jsx
+++ components/example/slider.jsx
@@ -2,22 +2,54 @@ import { useState } from "react"
 import { ChevronLeft, ChevronRight } from "react-iconly"
 
 const sliderItems = [
-  <div className="bg-orange-100">1</div>,
-  <div className="bg-orange-200">2</div>,
-  <div className="bg-orange-300">3</div>,
-  <div className="bg-orange-400">4</div>,
-  <div className="bg-orange-500">5</div>,
-  <div className="bg-orange-600">6</div>,
-  <div className="bg-orange-700">7</div>,
-  <div className="bg-orange-800">8</div>,
-  <div className="bg-orange-900">9</div>,
-  <div className="bg-orange-100">10</div>,
-  <div className="bg-orange-200">11</div>,
-  <div className="bg-orange-300">12</div>,
-  <div className="bg-orange-400">13</div>,
-  <div className="bg-orange-500">14</div>,
-  <div className="bg-orange-600">15</div>,
-  <div className="bg-orange-700">16</div>,
+  <div className="bg-orange-100 w-full" key={1}>
+    1
+  </div>,
+  <div className="bg-orange-200 w-full" key={2}>
+    2
+  </div>,
+  <div className="bg-orange-300 w-full" key={3}>
+    3
+  </div>,
+  <div className="bg-orange-400 w-full" key={4}>
+    4
+  </div>,
+  <div className="bg-orange-500 w-full" key={5}>
+    5
+  </div>,
+  <div className="bg-orange-600 w-full" key={6}>
+    6
+  </div>,
+  <div className="bg-orange-700 w-full" key={7}>
+    7
+  </div>,
+  <div className="bg-orange-800 w-full" key={8}>
+    8
+  </div>,
+  <div className="bg-orange-900 w-full" key={9}>
+    9
+  </div>,
+  <div className="bg-orange-100 w-full" key={10}>
+    10
+  </div>,
+  <div className="bg-orange-200 w-full" key={11}>
+    11
+  </div>,
+  <div className="bg-orange-300 w-full" key={12}>
+    12
+  </div>,
+  <div className="bg-orange-400 w-full" key={13}>
+    13
+  </div>,
+  <div className="bg-orange-500 w-full" key={14}>
+    14
+  </div>,
+  <div className="bg-orange-600 w-full" key={15}>
+    15
+  </div>,
+  <div className="bg-orange-700 w-full" key={16}>
+    16
+  </div>,
 ]
 
 const Slider = () => {
