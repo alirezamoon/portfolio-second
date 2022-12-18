fb95428fad0991f32e5f1fbec8a3834ac107e4e9
alireza khanamani <khanamani1998@gmail.com>
2022-12-18 16:33:33 +0330

    map key

--- components/ui/sidebarV2/menu.jsx
+++ components/ui/sidebarV2/menu.jsx
@@ -5,7 +5,7 @@ import MenuItem from "./menuItem"
 const Menu = () => {
   return (
     <div className="flex flex-col items-center">
-      {menuData.map((item, key) => (
+      {menuData.map((item, index) => (
         <MenuItem data={item} key={index} />
       ))}
     </div>
