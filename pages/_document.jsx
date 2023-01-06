983a733890bc47492141245f773157754e2b9130
alirezamoon <khanamani1998@gmail.com>
2023-01-06 11:41:13 -0800

    add title and meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" type="image/png" href="./a.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Alireza Khanamani</title>
       </Head>
       <body>
         <Main />
