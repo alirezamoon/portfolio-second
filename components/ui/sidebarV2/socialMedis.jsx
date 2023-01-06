84b6b6b47d2dbf3e7024c528a09f72acc1d55521
alirezamoon <khanamani1998@gmail.com>
2023-01-06 10:26:28 -0800

    telegram and linkedin

--- components/ui/sidebarV2/socialMedis.jsx
+++ components/ui/sidebarV2/socialMedis.jsx
@@ -1,7 +1,20 @@
-const SocialMedia = () =>{
-    return <div>
-        
+import Linkedin from "../svg/linkedin"
+import Telegram from "../svg/telegram"
+
+const SocialMedia = () => {
+  return (
+    <div className="flex flex-col gap-3 mx-auto mb-11 [&>a:hover_path]:fill-teal-main cursor-pointer">
+      <a href="https://t.me/alirez97/" target="_blank">
+        <Telegram />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/alireza-khanamani-aa84901b9/"
+        target="_blank"
+      >
+        <Linkedin />
+      </a>
     </div>
+  )
 }
 
 export default SocialMedia
