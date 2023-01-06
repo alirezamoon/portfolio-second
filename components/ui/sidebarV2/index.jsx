59fb2e689509b6d8f441b7785cd1f8d3f4f8534c
alirezamoon <khanamani1998@gmail.com>
2023-01-06 09:37:33 -0800

    add social media comp

--- components/ui/sidebarV2/index.jsx
+++ components/ui/sidebarV2/index.jsx
@@ -1,11 +1,13 @@
 import A from "../a"
 import Menu from "./menu"
+import SocialMedia from "./socialMedis"
 
 const Sidebar = () => {
   return (
     <div className="flex flex-col bg-gray-darker w-16 justify-between">
       <A className={"text-4xl mt-4"} />
       <Menu />
+      <SocialMedia />
     </div>
   )
 }
---
+++ components/ui/sidebarV2/socialMedis.jsx
@@ -0,0 +1,7 @@
+const SocialMedia = () =>{
+    return <div>
+        
+    </div>
+}
+
+export default SocialMedia
