d9d92c7169ae3cceaa3310385d3bf4f21498616c
alirezamoon <khanamani1998@gmail.com>
2023-01-08 23:49:48 -0800

    menu item

--- components/ui/sidebarV2/menuItem.jsx
+++ components/ui/sidebarV2/menuItem.jsx
@@ -32,32 +32,27 @@ const MenuItem = ({ data }) => {
   }, [data])
 
   return (
-    <div
-      className={`w-full duration-300 h-12 md:[&:hover>.menu-item-icon]:hidden md:[&:hover>.menu-item-text]:flex 
-                ${
-                  data.route !== "/"
-                    ? router.pathname.includes(data.route)
+    <Link
+      href={data.route}
+      className={`relative w-full h-12 flex justify-center items-center  
+                  md:[&:hover>p]:opacity-100 md:[&:hover>div]:opacity-0 [&:hover_path]:fill-teal-main duration-300 ${
+                    data.route !== "/"
+                      ? router.pathname.includes(data.route)
+                        ? " [&_path]:fill-teal-main"
+                        : ""
+                      : router.pathname === "/"
                       ? " [&_path]:fill-teal-main"
                       : ""
-                    : router.pathname === "/"
-                    ? " [&_path]:fill-teal-main"
-                    : ""
-                }`}
+                  }`}
     >
-      <Link
-        href={data.route}
-        className="relative w-full h-full flex justify-center items-center  
-                  [&:hover>p]:opacity-100 [&:hover>div]:opacity-0"
-      >
-        <div className="transition-opacity duration-300">{icon}</div>
-        <p
-          className="absolute top-1/2 -translate-y-1/2 w-full text-center opacity-0
+      <div className="transition-opacity duration-300">{icon}</div>
+      <p
+        className="absolute top-1/2 -translate-y-1/2 w-full text-center opacity-0
                      transition-opacity duration-300 text-teal-main text-sm"
-        >
-          {data.title}
-        </p>
-      </Link>
-    </div>
+      >
+        {data.title}
+      </p>
+    </Link>
   )
 }
 
