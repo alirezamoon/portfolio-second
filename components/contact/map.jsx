65aeb87f1ca23ea242e25958510b8a4fe8e7c98d
alirezamoon <khanamani1998@gmail.com>
2023-01-08 23:24:07 -0800

    url map

--- components/contact/map.jsx
+++ components/contact/map.jsx
@@ -22,8 +22,8 @@ const Map = () => {
   return (
     <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
       <TileLayer
-        attribution='<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
       />
       <Marker position={position} icon={iconPerson}>
         <Popup>I am here</Popup>
